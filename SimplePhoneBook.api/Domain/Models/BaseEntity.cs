ec6f875ec14c0e1e4d4051361286db03245a9bbb
Amiroo <amir.charkhand@outlook.com>
2026-08-18 12:57:14 +0330

    Add BaseEntity

---
+++ SimplePhoneBook.api/Domain/Models/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace SimplePhoneBook.api.Domain.Models;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; protected set; }
+    public DateTime CreatedDate { get; protected set; }
+
+    protected BaseEntity()
+    {
+        Id = Guid.NewGuid();
+        CreatedDate = DateTime.UtcNow;
+    }
+
+    protected BaseEntity(Guid id, DateTime createdDate)
+    {
+        Id = id;
+        CreatedDate = createdDate;
+    }
+}
