e9d5f113f2c2e2c1215835361d83a31adcbf086d
Amiroo <amir.charkhand@outlook.com>
2026-08-18 20:02:37 +0330

    Add IEntityRepository

---
+++ SimplePhoneBook.api/Data/PaginatedResult.cs
@@ -0,0 +1,10 @@
+﻿using SimplePhoneBook.api.Domain.Models;
+
+namespace SimplePhoneBook.api.Data;
+
+public class PaginatedResult<T> where T : BaseEntity
+{
+    public List<T>? items { get; init; }
+    public int currentPage { get; init; }
+    public int Total { get; init; }
+}
---
+++ SimplePhoneBook.api/Data/Repositories/Contracts/IEntityRepository.cs
@@ -0,0 +1,16 @@
+﻿using SimplePhoneBook.api.Domain.Models;
+
+namespace SimplePhoneBook.api.Data.Repositories.Contracts;
+
+public interface IEntityRepository<T> where T : BaseEntity
+{
+    public T GetById(Guid id);
+
+    public List<PaginatedResult<T>> GetAll(int page = 1, int pageSize = 10);
+
+    public void Create(T entity);
+
+    public void Update(T entity);
+
+    public void Delete(Guid id);
+}
