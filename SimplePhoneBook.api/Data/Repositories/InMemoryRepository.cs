492bbc760d2ed2166bc84cb7d3e4700881857e62
Amiroo <amir.charkhand@outlook.com>
2026-08-18 21:57:06 +0330

    Add Generic InMemoryRepository

---
+++ SimplePhoneBook.api/Data/Repositories/InMemoryRepository.cs
@@ -0,0 +1,54 @@
+﻿using SimplePhoneBook.api.Data.Repositories.Contracts;
+using SimplePhoneBook.api.Domain.Models;
+
+namespace SimplePhoneBook.api.Data.Repositories;
+
+public abstract class InMemoryRepository<T> : IEntityRepository<T> where T : BaseEntity
+{
+    protected List<T> Items = new List<T>();
+
+    public void Create(T entity)
+    {
+        Items.Add(entity);
+    }
+
+    public void Delete(Guid id)
+    {
+        var toBeDeleted = GetById(id);
+        if (toBeDeleted != null)
+            Items.Remove(toBeDeleted);
+    }
+
+    public PaginatedResult<T> GetAll(int page = 1, int pageSize = 10)
+    {
+        var skip = page - 1 * pageSize;
+        var items = Items
+            .OrderBy(x => x.CreatedDate)
+            .Skip(skip)
+            .Take(pageSize)
+            .ToList();
+
+        var totalCount = Items.Count();
+        var result = new PaginatedResult<T>
+        {
+            Items = items,
+            currentPage = page,
+            Total = totalCount
+        };
+
+        return result;
+    }
+
+    public T? GetById(Guid id)
+    {
+        var result = Items
+            .FirstOrDefault(x => x.Id == id);
+
+        return result;
+    }
+
+    public void Update(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
