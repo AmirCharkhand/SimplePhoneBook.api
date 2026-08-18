1813717ae28f95008e07eea0e16a851fde7e67fe
Amiroo <amir.charkhand@outlook.com>
2026-08-18 21:56:30 +0330

    Fix Items naming in Paginated Result

--- SimplePhoneBook.api/Data/PaginatedResult.cs
+++ SimplePhoneBook.api/Data/PaginatedResult.cs
@@ -4,7 +4,7 @@ namespace SimplePhoneBook.api.Data;
 
 public class PaginatedResult<T> where T : BaseEntity
 {
-    public List<T>? items { get; init; }
+    public List<T>? Items { get; init; }
     public int currentPage { get; init; }
     public int Total { get; init; }
 }
