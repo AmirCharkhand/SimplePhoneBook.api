990df295aab4e2e5232f5d766248296f858a003b
Amiroo <amir.charkhand@outlook.com>
2026-08-18 21:55:39 +0330

    Refactor IEntityRepository

--- SimplePhoneBook.api/Data/Repositories/Contracts/IEntityRepository.cs
+++ SimplePhoneBook.api/Data/Repositories/Contracts/IEntityRepository.cs
@@ -4,9 +4,9 @@ namespace SimplePhoneBook.api.Data.Repositories.Contracts;
 
 public interface IEntityRepository<T> where T : BaseEntity
 {
-    public T GetById(Guid id);
+    public T? GetById(Guid id);
 
-    public List<PaginatedResult<T>> GetAll(int page = 1, int pageSize = 10);
+    public PaginatedResult<T> GetAll(int page = 1, int pageSize = 10);
 
     public void Create(T entity);
 
