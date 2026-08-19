b0e8d1862cdfd1b7045b9a1537e9ca65ff395974
Amiroo <amir.charkhand@outlook.com>
2026-08-19 18:07:05 +0330

    Develop InvalidTagReferenceException

---
+++ SimplePhoneBook.api/Application/Exceptions/InvalidTagReferenceException.cs
@@ -0,0 +1,9 @@
+﻿namespace SimplePhoneBook.api.Application.Exceptions;
+
+public class InvalidTagReferenceException : Exception
+{
+    public InvalidTagReferenceException(Guid tagId)
+        : base($"Tag with id '{tagId}' does not exist.")
+    {
+    }
+}
