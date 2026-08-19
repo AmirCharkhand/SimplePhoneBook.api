3e8161fe9f016173923e09b442e096078d85a9cf
Amiroo <amir.charkhand@outlook.com>
2026-08-19 18:37:33 +0330

    Refactor TagController to use the TagService

--- SimplePhoneBook.api/Controllers/TagController.cs
+++ SimplePhoneBook.api/Controllers/TagController.cs
@@ -1,56 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SimplePhoneBook.api.Application.DTOs.TagDTOs;
-using SimplePhoneBook.api.Domain.Repositories;
+using SimplePhoneBook.api.Application.Services.Interfaces;
 
 namespace SimplePhoneBook.api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TagController (ITagRepository tagRepository): ControllerBase
+    public class TagController(ITagService tagService) : ControllerBase
     {
         [HttpGet]
         public IActionResult GetTags()
         {
-            var tags = tagRepository.GetAll();
-            var dtos = tags.Select(t => t.ToResponseDto()).ToList();
+            var dtos = tagService.GetAll();
             return Ok(dtos);
         }
 
         [HttpGet("{id:guid}")]
         public IActionResult GetTag(Guid id)
         {
-            var tag = tagRepository.GetById(id);
-            
-            if (tag == null)
+            var dto = tagService.GetById(id);
+
+            if (dto == null)
                 return NotFound();
 
-            return Ok(tag.ToResponseDto());
+            return Ok(dto);
         }
 
         [HttpPost]
         public IActionResult CreateTag(TagRequestDto tagDto)
         {
-            var tag = tagDto.ToTag();
-            tagRepository.Create(tag);
-            return Ok(tag.ToResponseDto());
+            var dto = tagService.Create(tagDto);
+            return CreatedAtAction(nameof(GetTag), new { id = dto.Id }, dto);
         }
 
         [HttpPut("{id:guid}")]
         public IActionResult UpdateTag(Guid id, TagRequestDto tagDto)
         {
-            var existingTag = tagRepository.GetById(id);
-            if (existingTag == null)
-                return NotFound();
-
-            tagDto.ApplyTo(existingTag);
-            tagRepository.Update(existingTag);
-            return Ok(existingTag.ToResponseDto());
+            var dto = tagService.Update(id, tagDto);
+            return Ok(dto);
         }
 
-        [HttpDelete]
+        [HttpDelete("{id:guid}")]
         public IActionResult DeleteTag(Guid id)
         {
-            tagRepository.Delete(id);
+            tagService.Delete(id);
             return NoContent();
         }
     }
